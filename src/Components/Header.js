a1ef04f4f6221205ba113fa664293cdb3091b337
Alexbw9934 <alexbw9934@outlook.com>
2022-07-11 15:15:29 +0900

    commit

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,16 +1,25 @@
 import React from 'react';
 import { useLocation } from 'react-router-dom';
 import {
+    AppBar,
     CssBaseline,
-    CustomAppbar
 } from '@mui/material';
 import {styled, useTheme} from '@mui/material/styles';
 
+const CustomAppBar = styled(AppBar, {
+    shouldForwardProp: prop => prop !== 'open',
+})(({ theme, open }) => {
+    zIndex: theme.zIndex.drawer + 1,
 
+});
 
 function Header() {
+    const theme = useTheme();
     <>
         <CssBaseline />
+        <CustomAppBar>
+
+        </CustomAppBar>
     </>
 }
 
