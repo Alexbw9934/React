7da4f9afccb8f3c794b2521b078ccee0ca61eba5
Alexbw9934 <alexbw9934@outlook.com>
2022-07-13 11:03:24 +0900

    alex

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -8,9 +8,14 @@ import {
     Typography,
     Box,
     ClickAwayListener,
+    InputBase,
+    Menu,
+    MenuItem,
 } from '@mui/material';
 import MenuIcon from '@mui/icons-material/Menu';
-import {styled, useTheme} from '@mui/material/styles';
+import SearchIcon from '@mui/icons-material/Search';
+import AccountCircle from '@mui/icons-material/AccountCircle'
+import {styled, useTheme, alpha } from '@mui/material/styles';
 
 const pages = [
     {
@@ -55,16 +60,77 @@ const CustomAppBar = styled(AppBar, {
     })
 }));
 
+const Search = styled('div')(({ theme }) => ({
+    position: 'relative',
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: alpha(theme.palette.common.white, 0.15),
+    '&:hover': {
+        backgroundColor: alpha(theme.palette.common.white, 0.25),
+        },
+        marginLeft: 0,
+        width: '100%',
+        [theme.breakpoints.up('sm')]: {
+        marginLeft: theme.spacing(1),
+        width: 'auto',
+        },
+  }));
+  
+const SearchIconWrapper = styled('div')(({ theme }) => ({
+    padding: theme.spacing(0, 2),
+    height: '100%',
+    position: 'absolute',
+    pointerEvents: 'none',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+}));
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+    color: 'inherit',
+    '& .MuiInputBase-input': {
+        padding: theme.spacing(1, 1, 1, 0),
+        // vertical padding + font size from searchIcon
+        paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+        transition: theme.transitions.create('width'),
+        width: '100%',
+        [theme.breakpoints.up('md')]: {
+        width: '12ch',
+        '&:focus': {
+            width: '20ch',
+        },
+        },
+    },
+}));
+
 function Header() {
     const theme = useTheme();
     const location = useLocation();
     const [open, sidebarToggle] = useState(false);
+    const [auth, statusAuth] = useState(true);
+    const [anchorEl, setAnchorEl] = useState(null);
 
     const handleSideBar = event => {
         event.preventDefault();
         event.stopPropagation();
         sidebarToggle(true);
     }
+
+    const handleAuth = event => {
+        statusAuth(ture);
+    }
+
+    const handleLogout = () => {
+        statusAuth(false);
+        setAnchorEl(null);
+    }
+
+    const handleMenu = (event) => {
+        setAnchorEl(event.currentTarget);
+      };
+    
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
 
     return (
         <>
@@ -74,7 +140,7 @@ function Header() {
                 open = {open} 
                 className="bg-gradient-to-r from-violet-500 to-fuchsia-500 !mb-5"
             >
-                <Toolbar className="!float-right">
+                <Toolbar>
                     {!open && (
                         <IconButton
                             size="large"
@@ -94,8 +160,8 @@ function Header() {
                             component="a"
                             href="/"
                             sx={{
-                            mr: 2,
-                                display: { xs: 'none', md: 'flex' },
+                                mr: 2,
+                                display: { xs: 'none', md: 'block' },
                                 flexGrow: 1,
                                 fontFamily: 'monospace',
                                 fontWeight: 700,
@@ -107,25 +173,15 @@ function Header() {
                             LOGO
                         </Typography>
                     )}
-                    <Typography
-                        variant="h5"
-                        noWrap
-                        component="a"
-                        href="/"
-                        sx={{
-                        mr: 2,
-                            display: { xs: 'flex', md: 'none' },
-                            flexGrow: 1,
-                            justifyContent: 'center',
-                            fontFamily: 'monospace',
-                            fontWeight: 700,
-                            letterSpacing: '.3rem',
-                            color: 'inherit',
-                            textDecoration: 'none',
-                        }}
-                    >
-                        LOGO
-                    </Typography>
+                    <Search>
+                        <SearchIconWrapper>
+                            <SearchIcon />
+                        </SearchIconWrapper>
+                        <StyledInputBase
+                            placeholder="Search…"
+                            inputProps={{ 'aria-label': 'search' }}
+                        />
+                    </Search>
                     <Box
                         sx={{
                             flexGrow: 1,
@@ -135,15 +191,6 @@ function Header() {
                         <nav className="ml-auto">
                             <ul>
                                 {pages.map((page, index) => (
-                                    // <Button
-                                    //     key={page}
-                                    //     component="a"
-                                    //     href={`/${page.toLowerCase()}`}
-                                    //     onClick={handleCloseNavMenu}
-                                    //     sx={{ my: 2, color: 'white', display: 'block' }}
-                                    // >
-                                    //     {page}
-                                    // </Button>
                                     <NavLink to={page.url} key={index} className="mr-2">
                                         {page.name}
                                     </NavLink>
@@ -151,6 +198,39 @@ function Header() {
                             </ul>
                         </nav>
                     </Box>
+                    {auth && (
+                        <div className='ml-auto'>
+                            <IconButton
+                                size="large"
+                                aria-label="account of current user"
+                                aria-controls="menu-appbar"
+                                aria-haspopup="true"
+                                onClick={handleMenu}
+                                color="inherit"
+                            >
+                                <AccountCircle />
+                            </IconButton>
+                            <Menu
+                                id="menu-appbar"
+                                anchorEl={anchorEl}
+                                anchorOrigin={{
+                                    vertical: 'top',
+                                    horizontal: 'right',
+                                }}
+                                keepMounted
+                                transformOrigin={{
+                                    vertical: 'top',
+                                    horizontal: 'right',
+                                }}
+                                open={Boolean(anchorEl)}
+                                onClose={handleClose}
+                            >
+                                <MenuItem onClick={handleClose}>Profile</MenuItem>
+                                <MenuItem onClick={handleClose}>My account</MenuItem>
+                                <MenuItem onClick={handleLogout}>Log out</MenuItem>
+                            </Menu>
+                        </div>
+                    )}
                 </Toolbar>
             </CustomAppBar>
         </>
